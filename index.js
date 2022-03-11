8ecc1d638ebd187af10a9d351f9ca65616d0ee16
anindyo <anindyo0216@gmail.com>
2022-03-11 15:17:16 +0700

    add some error
    Signed-off-by: anindyo <anindyo0216@gmail.com>

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var port = 8080;
 http.createServer(function (req, res) {
 	console.log("Aplikasi berjalan pada port : "+port);
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('HELPPPP FIXED');
+  res.end('HELPPPP' pls);
 }).listen(port);
